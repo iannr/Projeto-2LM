b18b0c61098651ea944ea32cd6e8f7104fc5cb73
Ian Nascimento Rocha <ian18nr@gmail.com>
2023-10-10 15:25:58 -0300

    Criando Classe de Recompensa

---
+++ XuBank/src/SistemaRecompensas.java
@@ -0,0 +1,28 @@
+public class SistemaRecompensas {
+	 private int pontos;
+
+	    public SistemaRecompensas() {
+	        this.pontos = 0;
+	    }
+
+	    // Método para acumular pontos
+	    public void acumularPontos(int quantidade) {
+	        pontos += quantidade;
+	    }
+
+	    // Método para verificar o saldo de pontos
+	    public int getSaldoPontos() {
+	        return pontos;
+	    }
+
+	    // Método para resgatar pontos por prêmios, descontos, ou conversão em dinheiro
+	    public void resgatarPontos(int quantidade) {
+	        if (pontos >= quantidade) {
+	            pontos -= quantidade;
+	            // Implemente aqui a lógica para conceder prêmios, descontos ou conversão em dinheiro.
+	        } else {
+	            System.out.println("Saldo insuficiente de pontos.");
+	        }
+	    }
+
+}

8b57c880a04a3b3b5963f9ced81a5c75c9094c04
Saulo Pereira Vasconcelos <saulovasconcelos07@gmail.com>
2023-10-10 15:28:51 -0300

    Criando Classe ClienteVip

--- XuBank/src/ClienteVip.java
+++ XuBank/src/ClienteVip.java
@@ -1,4 +1,26 @@
 
-public class ClienteVip {
+class ClienteVip extends Cliente {
+    private int pontosFidelidade;
+    private SistemaRecompensas sistemaRecompensas;
 
+    public ClienteVip(String nome, String cpf, String senha) {
+        super(nome, cpf, senha);
+        this.pontosFidelidade = 0;
+        this.sistemaRecompensas = new SistemaRecompensas();
+    }
+
+    // Métodos específicos para o cliente VIP
+    public void acumularPontosFidelidade(double saldo) {
+        int pontosGanhos = (int) (saldo / 2000) * 30;
+        pontosFidelidade += pontosGanhos;
+        sistemaRecompensas.acumularPontos(pontosGanhos);
+    }
+
+    public void verificarSaldoFidelidade() {
+        System.out.println("Pontos de fidelidade: " + pontosFidelidade);
+    }
+
+    public void resgatarRecompensas(int quantidadePontos) {
+        sistemaRecompensas.resgatarPontos(quantidadePontos);
+    }
 }

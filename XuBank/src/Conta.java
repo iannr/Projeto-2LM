28e5fe6a6a350324e75ace8a7535bfdd9593d057
Saulo Pereira Vasconcelos <saulovasconcelos07@gmail.com>
2023-10-10 15:17:39 -0300

    Alterando Classe Cliente e Implementando Classe Conta

--- XuBank/src/Conta.java
+++ XuBank/src/Conta.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class Conta {
+abstract class Conta {
+    private double saldo;
+    private Cliente cliente;
+    private List<String> extrato;
 
+    public Conta(Cliente cliente) {
+        this.cliente = cliente;
+        this.saldo = 0;
+        this.extrato = new ArrayList<>();
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<String> getExtrato() {
+        return extrato;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+        extrato.add("Depósito: +" + valor);
+    }
+
+    // Método abstrato para sacar (será implementado nas subclasses)
+    public abstract void sacar(double valor);
+
+    // Método para transferir entre contas
+    public void transferir(Conta destino, double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            destino.depositar(valor);
+            extrato.add("Transferência para " + destino.getClass().getSimpleName() + ": -" + valor);
+            destino.getExtrato().add("Transferência de " + getClass().getSimpleName() + ": +" + valor);
+        } else {
+            System.out.println("Saldo insuficiente para transferência.");
+        }
+    }
 }

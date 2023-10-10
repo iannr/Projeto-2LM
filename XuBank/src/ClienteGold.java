8c443e9062c321be78e6a341cddd61337d7fda31
Saulo Pereira Vasconcelos <saulovasconcelos07@gmail.com>
2023-10-10 15:26:57 -0300

    Criando e Implementando Classe ClienteGold

--- XuBank/src/ClienteGold.java
+++ XuBank/src/ClienteGold.java
@@ -1,4 +1,25 @@
 
-public class ClienteGold {
+class ClienteGold extends Cliente {
+    private int pontosFidelidade;
+    private SistemaRecompensas sistemaRecompensas;
 
+    public ClienteGold(String nome, String cpf, String senha) {
+        super(nome, cpf, senha);
+        this.pontosFidelidade = 0;
+        this.sistemaRecompensas = new SistemaRecompensas();
+    }
+
+    // Métodos específicos para o cliente Gold
+    public void acumularPontosFidelidade() {
+        pontosFidelidade += 10;
+        sistemaRecompensas.acumularPontos(10);
+    }
+
+    public void verificarSaldoFidelidade() {
+        System.out.println("Pontos de fidelidade: " + pontosFidelidade);
+    }
+
+    public void resgatarRecompensas(int quantidadePontos) {
+        sistemaRecompensas.resgatarPontos(quantidadePontos);
+    }
 }

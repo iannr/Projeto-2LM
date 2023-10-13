63471ce4d3ef931a38d14451124ebe44aa4efaa9
Raul Fernandes <103540662+RaulFernandesGoulart@users.noreply.github.com>
2023-10-12 21:05:40 -0300

    ContaCorrente

---
+++ XuBank/src/ContaCorrente.java
@@ -0,0 +1,33 @@
+public class ContaCorrente {
+    private double saldo;
+    
+    public ContaCorrente(double saldoInicial) {
+        saldo = saldoInicial;
+    }
+    
+    public void cobrarTaxaMensal() {
+        saldo -= 20.0;
+    }
+    
+    public boolean realizarSaqueEspecial(double valor) {
+        if (valor > 0 && valor <= (saldo + 200.0)) {
+            saldo -= valor;
+            return true;
+        } else {
+            System.out.println("Saque especial excedeu o limite de R$200 ou é um valor inválido.");
+            return false;
+        }
+    }
+    
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        } else {
+            System.out.println("O valor de depósito deve ser maior que zero.");
+        }
+    }
+    
+    public double getSaldo() {
+        return saldo;
+    }
+}
